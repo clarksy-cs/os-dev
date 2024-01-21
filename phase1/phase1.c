bdc5a734185ffdb1c10eaf07b7bc089bb38bf033
clarksy <anthony.cl@live.com>
2024-01-20 18:20:55 -0800

    added TODOs w extra functions/modified fork1

--- phase1/phase1.c
+++ phase1/phase1.c
@@ -11,23 +11,26 @@
 #include "kernel.h"
 
 /* ------------------------- Prototypes ----------------------------------- */
-int sentinel (void *);
+int sentinel (char *);
 extern int start1 (char *);
 void dispatcher(void);
 void launch();
 static void enableInterrupts();
 static void check_deadlock();
+void add_to_ready_list(proc_struct *next_ready_proc);
+proc_ptr get_next_ready_proc();
+proc_ptr pop_proc();
 
 
 /* -------------------------- Globals ------------------------------------- */
 
-/* Patrick's debugging global variable... */
+/* debugging global variable... */
 int debugflag = 1;
 
 /* the process table */
 proc_struct ProcTable[MAXPROC];
 
-/* Process lists  */
+/* process lists */
 
 /* current process ID */
 proc_ptr Current;
@@ -35,27 +38,41 @@ proc_ptr Current;
 /* the next pid to be assigned */
 unsigned int next_pid = SENTINELPID;
 
+/* initiates dispatch */
+int ready_to_start = 0;
+
+/* ready list */
+proc_ptr ready_list[6];
+
 
 /* -------------------------- Functions ----------------------------------- */
 /* ------------------------------------------------------------------------
    Name - startup
    Purpose - Initializes process lists and clock interrupt vector.
-	     Start up sentinel process and the test process.
+	          Start up sentinel process and the test process.
    Parameters - none, called by USLOSS
    Returns - nothing
    Side Effects - lots, starts the whole thing
    ----------------------------------------------------------------------- */
-void startup()
-{
-   int i;      /* loop index */
+void startup() {
+
    int result; /* value returned by call to fork1() */
 
    /* initialize the process table */
+   for (int i = 0; i < MAXPROC; i++) {
+      ProcTable[i].next_proc_ptr = NULL;
+      ProcTable[i].child_proc_ptr = NULL;
+      ProcTable[i].next_sibling_ptr = NULL;
+      ProcTable[i].pid = -1;
+      ProcTable[i].priority = 0;
+      ProcTable[i].start_func = NULL;
+      ProcTable[i].stack = NULL;
+      ProcTable[i].stacksize = 0;
+      ProcTable[i].status = 0;
+   }
 
-   /* Initialize the Ready list, etc. */
    if (DEBUG && debugflag)
       console("startup(): initializing the Ready & Blocked lists\n");
-   //ReadyList = NULL;
 
    /* Initialize the clock interrupt handler */
 
@@ -69,6 +86,9 @@ void startup()
          console("startup(): fork1 of sentinel returned error, halting...\n");
       halt(1);
    }
+
+   /* initiate dispatch in fork1 */
+   ready_to_start = 1;
   
    /* start the test process */
    if (DEBUG && debugflag)
@@ -92,8 +112,8 @@ void startup()
    Returns - nothing
    Side Effects - none
    ----------------------------------------------------------------------- */
-void finish()
-{
+void finish() {
+
    if (DEBUG && debugflag)
       console("in finish...\n");
 } /* finish */
@@ -110,8 +130,8 @@ void finish()
    Side Effects - ReadyList is changed, ProcTable is changed, Current
                   process information changed
    ------------------------------------------------------------------------ */
-int fork1(char *name, int (*f)(char *), char *arg, int stacksize, int priority)
-{
+int fork1(char *name, int (*f)(char *), char *arg, int stacksize, int priority) {
+
    int proc_slot;
 
    if (DEBUG && debugflag)
@@ -119,29 +139,52 @@ int fork1(char *name, int (*f)(char *), char *arg, int stacksize, int priority)
 
    /* test if in kernel mode; halt if in user mode */
 
-   /* Return if stack size is too small */
+   /* return if stack size is too small */
+   if (stacksize < USLOSS_MIN_STACK) {
+      return;
+   }
 
    /* find an empty slot in the process table */
+   for (int i = 0; i < MAXPROC; i++) {
+      if (ProcTable[i].pid == -1) {
+         proc_slot = i;
+         break;
+      }
+      if (i == MAXPROC - 1) {
+         console("fork1(): Exceeded max processes. Halting...\n");
+         halt(1);
+      }
+   }
 
    /* fill-in entry in process table */
-   if ( strlen(name) >= (MAXNAME - 1) ) {
-      console("fork1(): Process name is too long.  Halting...\n");
+   if (strlen(name) >= (MAXNAME - 1)) {
+      console("fork1(): Process name is too long. Halting...\n");
       halt(1);
    }
    strcpy(ProcTable[proc_slot].name, name);
+
+   /* save the entry point for the new process */
    ProcTable[proc_slot].start_func = f;
+
+   /* allocate stack */
+   ProcTable[proc_slot].stacksize = stacksize;
+   ProcTable[proc_slot].stack = malloc(stacksize);
+
+   /* set pid */
+   ProcTable[proc_slot].pid = next_pid;
+   next_pid++;
+
    if ( arg == NULL )
       ProcTable[proc_slot].start_arg[0] = '\0';
-   else if ( strlen(arg) >= (MAXARG - 1) ) {
-      console("fork1(): argument too long.  Halting...\n");
+   else if (strlen(arg) >= (MAXARG - 1)) {
+      console("fork1(): Argument too long. Halting...\n");
       halt(1);
-   }
-   else
+   } else {
       strcpy(ProcTable[proc_slot].start_arg, arg);
+   }
 
    /* Initialize context for this process, but use launch function pointer for
-    * the initial value of the process's program counter (PC)
-    */
+    * the initial value of the process's program counter (PC) */
    context_init(&(ProcTable[proc_slot].state), psr_get(),
                 ProcTable[proc_slot].stack, 
                 ProcTable[proc_slot].stacksize, launch);
@@ -149,7 +192,16 @@ int fork1(char *name, int (*f)(char *), char *arg, int stacksize, int priority)
    /* for future phase(s) */
    p1_fork(ProcTable[proc_slot].pid);
 
-} /* fork1 */
+   /* add to ready list */
+   add_to_ready_list(&ProcTable[proc_slot]);
+
+   // TODO:
+   if (ready_to_start) {
+      dispatcher();
+   }
+
+
+}  /* fork1 */
 
 /* ------------------------------------------------------------------------
    Name - launch
@@ -192,8 +244,8 @@ void launch()
    Side Effects - If no child process has quit before join is called, the 
                   parent is removed from the ready list and blocked.
    ------------------------------------------------------------------------ */
-int join(int *code)
-{
+int join(int *code) {
+
 } /* join */
 
 
@@ -206,11 +258,31 @@ int join(int *code)
    Returns - nothing
    Side Effects - changes the parent of pid child completion status list.
    ------------------------------------------------------------------------ */
-void quit(int code)
-{
+void quit(int code) {
+
    p1_quit(Current->pid);
 } /* quit */
 
+proc_ptr get_next_ready_proc() {
+   // TODO:
+   // pop next entry from the ready list
+   for (int i = 0; i < SENTINELPRIORITY; i++) {
+   // if list is not empty
+      if (ready_list[i] != NULL) {
+         // TODO:
+         // pop list head from correlating process priority
+         proc_ptr ready_proc = pop_proc();
+         return ready_proc;
+      }
+   }
+   return NULL;
+}
+
+void add_to_ready_list(proc_struct *new_ready_proc) {
+   // TODO:
+   // add entry to ready list
+   ready_list[new_ready_proc->priority - 1];
+}
 
 /* ------------------------------------------------------------------------
    Name - dispatcher
@@ -222,11 +294,22 @@ void quit(int code)
    Returns - nothing
    Side Effects - the context of the machine is changed
    ----------------------------------------------------------------------- */
-void dispatcher(void)
-{
+void dispatcher(void) {
+
    proc_ptr next_process;
+   context *prev_context_ptr = NULL;
 
    p1_switch(Current->pid, next_process->pid);
+
+   // find the next process to run
+   next_process = get_next_ready_proc();
+
+   // set current ptr to new process
+   Current = next_process;
+
+   // swap old process with new process
+   context_switch(prev_context_ptr, &Current->state);
+
 } /* dispatcher */
 
 
@@ -241,8 +324,8 @@ void dispatcher(void)
    Side Effects -  if system is in deadlock, print appropriate error
 		   and halt.
    ----------------------------------------------------------------------- */
-int sentinel (void * dummy)
-{
+int sentinel (char *dummy) {
+
    if (DEBUG && debugflag)
       console("sentinel(): called\n");
    while (1)
@@ -254,17 +337,15 @@ int sentinel (void * dummy)
 
 
 /* check to determine if deadlock has occurred... */
-static void check_deadlock()
-{
+static void check_deadlock() {
+
 
 } /* check_deadlock */
 
 
-/*
- * Disables the interrupts.
- */
-void disableInterrupts()
-{
+/* Disables the interrupts. */
+void disableInterrupts() {
+
   /* turn the interrupts OFF iff we are in kernel mode */
   if((PSR_CURRENT_MODE & psr_get()) == 0) {
     //not in kernel mode
