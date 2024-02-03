3d4f8d7021dc730cd8ede8a0ca5cae44e878680e
clarksy <anthony.cl@live.com>
2024-02-03 12:59:49 -0800

    passes test00/01

--- phase1/phase1.c
+++ phase1/phase1.c
@@ -14,14 +14,18 @@
 int sentinel (char *);
 extern int start1 (char *);
 void dispatcher(void);
-void launch();
-static void enableInterrupts();
-static void check_deadlock();
+void launch(void);
+static void enableInterrupts(void);
+static void check_deadlock(void);
 void add_ready_proc(proc_struct *next_ready_proc);
-proc_ptr get_ready_proc();
+proc_ptr get_ready_proc(void);
 void list_add_node(List *current_proc, proc_ptr new_node);
 proc_ptr list_pop_node(List *current_proc);
 void clear_proc_entry(int target);
+void check_kernel_mode(void);
+void clock_handler(int dev, void *arg);
+void dump_processes(void);
+int getpid(void);
 
 /* -------------------------- Globals ------------------------------------- */
 
@@ -43,6 +47,9 @@ unsigned int new_pid = SENTINELPID;
 /* initiates dispatch */
 int start_flag = 1;
 
+/* process count */
+int num_proc = 0;
+
 /* -------------------------- Functions ----------------------------------- */
 /* ------------------------------------------------------------------------
    Name - startup
@@ -52,7 +59,7 @@ int start_flag = 1;
    Returns - nothing
    Side Effects - lots, starts the whole thing
    ----------------------------------------------------------------------- */
-void startup() {
+void startup(void) {
 
    int result; /* value returned by call to fork1() */
 
@@ -88,6 +95,7 @@ void startup() {
       console("startup(): initializing the Ready & Blocked lists\n");
 
    /* Initialize the clock interrupt handler */
+   int_vec[CLOCK_INT] = clock_handler;
 
    /* startup a sentinel process */
    if (DEBUG && debugflag)
@@ -131,7 +139,7 @@ void startup() {
    Returns - nothing
    Side Effects - none
    ----------------------------------------------------------------------- */
-void finish() {
+void finish(void) {
 
    if (DEBUG && debugflag)
       console("in finish...\n");
@@ -153,7 +161,7 @@ int fork1(char *name, int (*f)(char *), char *arg, int stacksize, int priority) 
 
    int proc_slot;
 
-   // TEST CODE ********* POSSIBLE SOLUTION
+   // if starting
    if (proc_tbl[1].pid == 2) {
       start_flag = 1;
    }
@@ -162,7 +170,7 @@ int fork1(char *name, int (*f)(char *), char *arg, int stacksize, int priority) 
       console("fork1(): creating process %s\n", name);
 
    /* test if in kernel mode; halt if in user mode */
-   // TODO:
+   check_kernel_mode();
 
    /* return if stack size is too small */
    if (stacksize < USLOSS_MIN_STACK) {
@@ -201,6 +209,7 @@ int fork1(char *name, int (*f)(char *), char *arg, int stacksize, int priority) 
    new_proc->status = STATUS_READY;
 
    new_pid++;
+   num_proc++;
 
    if ( arg == NULL )
       new_proc->start_arg[0] = '\0';
@@ -214,8 +223,6 @@ int fork1(char *name, int (*f)(char *), char *arg, int stacksize, int priority) 
    if (current != NULL && current->pid != 1) {
       list_add_node(&current->children, new_proc);
       new_proc->parent_proc_ptr = current;
-      // TEST //
-      current->status = STATUS_JOIN_BLOCKED;
    }
 
    /* Initialize context for this process, but use launch function pointer for
@@ -270,6 +277,9 @@ proc_ptr list_pop_node(List *current_proc) {
    }
 
    current_proc->count--;
+   if (current_proc->count < 0) {
+      current_proc->count = 0;
+   }
 
    return rm_node;
 }
@@ -282,7 +292,7 @@ proc_ptr list_pop_node(List *current_proc) {
    Returns - nothing
    Side Effects - enable interrupts
    ------------------------------------------------------------------------ */
-void launch()
+void launch(void)
 {
    int result;
 
@@ -302,7 +312,6 @@ void launch()
 
 } /* launch */
 
-
 /* ------------------------------------------------------------------------
    Name - join
    Purpose - Wait for a child process (if one has been forked) to quit.  If 
@@ -317,33 +326,36 @@ void launch()
    ------------------------------------------------------------------------ */
 int join(int *code) {
 
-   // ******WORK IN PROGRESS *************
-   // TODO: figure out how to get start1 to loop here until
-   //       all children quit
-
-   // check if any child process has already quit
-   proc_ptr child = current->quitting_children.p_head;
+   proc_ptr parent = NULL;
+   proc_ptr child = NULL;
    int child_pid = 0;
 
-   while (child != NULL) {
-      if (child->status == -3) {
+   // store calling process in parent variable
+   if (current->parent_proc_ptr == NULL) {
+      parent = current;
+   }
+
+   while (parent->children.count > 0 || parent->quitting_children.count > 0) {
+
+      if (parent->quitting_children.p_head != NULL) {
+         child = parent->quitting_children.p_head;
          // child has quit, set termination code
          *code = child->status;
 
          // remove child from the children list
-         child = list_pop_node(&current->quitting_children);
+         child = list_pop_node(&parent->quitting_children);
          child_pid = child->pid;
          clear_proc_entry(child_pid);
          return child_pid;
       }
-      child = child->next_proc_ptr;
+
+      // no child process has quit, block the parent
+      parent->status = STATUS_JOIN_BLOCKED;
+      list_pop_node(&ready_procs[parent->priority - 1]);
+      dispatcher();
    }
 
-   // no child process has quit, block the parent
-   current->status = STATUS_JOIN_BLOCKED;
-   dispatcher();
-
-
+   // parent has no children
    return -2;
 
 } /* join */
@@ -360,10 +372,6 @@ int join(int *code) {
    ------------------------------------------------------------------------ */
 void quit(int code) {
 
-   if (code == 0) {
-      exit(0);
-   }
-   
    current->status = STATUS_QUIT;
 
    /* find parent process */
@@ -378,6 +386,10 @@ void quit(int code) {
       add_ready_proc(current->parent_proc_ptr);
    }
 
+   if (current->parent_proc_ptr == NULL) {
+      clear_proc_entry(current->pid);
+   }
+
    dispatcher();
    p1_quit(current->pid);
    enableInterrupts();
@@ -387,9 +399,14 @@ void quit(int code) {
 proc_ptr get_ready_proc(void) {
 
    for (int i = 0; i <= MINPRIORITY; i++) {
-   
       if (ready_procs[i].count > 0) {
          /* if list is not empty */
+         if (ready_procs[i].p_head->parent_proc_ptr != NULL 
+         && ready_procs[i].p_head->parent_proc_ptr->priority < ready_procs[i].p_head->priority
+         && ready_procs[i].p_head->parent_proc_ptr->status == STATUS_RUNNING) {
+            /* return NULL if parent process is running and higher priority */
+            return NULL;
+         }
          proc_ptr ready_proc = list_pop_node(&ready_procs[i]);
          return ready_proc;
       }
@@ -406,22 +423,30 @@ void add_ready_proc(proc_ptr new_ready_proc) {
 }
 
 void clear_proc_entry(int target) {
-   proc_tbl[target].next_proc_ptr = NULL;
-   proc_tbl[target].prev_proc_ptr = NULL;
-   proc_tbl[target].next_sibling_ptr = NULL;
-   proc_tbl[target].prev_sibling_ptr = NULL;
-   proc_tbl[target].parent_proc_ptr = NULL;
-   proc_tbl[target].pid = 0;
-   proc_tbl[target].priority = 0;
-   proc_tbl[target].start_func = NULL;
-   proc_tbl[target].stack = NULL;
-   proc_tbl[target].stacksize = 0;
-   proc_tbl[target].status = STATUS_EMPTY;
-   proc_tbl[target].children.p_head = NULL;
-   proc_tbl[target].children.p_tail = NULL;
-   proc_tbl[target].children.count = 0;
-   proc_tbl[target].children.offset = 0;
+   
+   for (int i = 0; i < MAXPROC; i++) {
+      if (proc_tbl[i].pid == target) {
+         target = i;
 
+         proc_tbl[target].next_proc_ptr = NULL;
+         proc_tbl[target].prev_proc_ptr = NULL;
+         proc_tbl[target].next_sibling_ptr = NULL;
+         proc_tbl[target].prev_sibling_ptr = NULL;
+         proc_tbl[target].parent_proc_ptr = NULL;
+         proc_tbl[target].pid = 0;
+         proc_tbl[target].priority = 0;
+         proc_tbl[target].start_func = NULL;
+         proc_tbl[target].stack = NULL;
+         proc_tbl[target].stacksize = 0;
+         proc_tbl[target].status = STATUS_EMPTY;
+         proc_tbl[target].children.p_head = NULL;
+         proc_tbl[target].children.p_tail = NULL;
+         proc_tbl[target].children.count = 0;
+         proc_tbl[target].children.offset = 0;
+      }
+   }
+
+   num_proc--;
 }
 
 /* ------------------------------------------------------------------------
@@ -439,6 +464,7 @@ void dispatcher(void) {
    proc_ptr next_process;
    context *prev_context_ptr;
 
+   /* null previous contect ptr for first process */
    if (current->pid == 1) {
       prev_context_ptr = NULL;
    } else {
@@ -487,14 +513,26 @@ int sentinel (char *dummy) {
 
 
 /* check to determine if deadlock has occurred... */
-static void check_deadlock() {
+static void check_deadlock(void) {
 
+   /* TODO: *********************
+   if (check_io() == 1) {
+      return;
+   } */
 
+   /* sentinel is the only active process */
+   if (num_proc == 1) {
+      console("All processes completed.\n");
+      halt(0);
+   }
+
+   console("check_deadlock(): Processes active: %d -- Halting...\n", num_proc);
+   halt(1);
 } /* check_deadlock */
 
 
 /* Disables the interrupts. */
-void disableInterrupts() {
+void disableInterrupts(void) {
 
   /* turn the interrupts OFF iff we are in kernel mode */
   if((PSR_CURRENT_MODE & psr_get()) == 0) {
@@ -506,6 +544,73 @@ void disableInterrupts() {
     psr_set( psr_get() & ~PSR_CURRENT_INT );
 } /* disableInterrupts */
 
-static void enableInterrupts() {
+static void enableInterrupts(void) {
    // TODO: write code for this function
+}
+
+void clock_handler(int dev, void *arg) {
+   // TODO: write code for this function
+}
+
+/* check if process is running in kernel mode */
+void check_kernel_mode(void) {
+   // psr_get() returns current processes mode: 1 = kernel mode, 0 = user mode.
+   unsigned int psr = psr_get();
+   if ((psr & PSR_CURRENT_MODE) == 0) {
+      console("fork1(): Not in kernel mode. Halting...\n");
+      halt(1);
+   }
+}
+
+/* prints process table info to console */
+void dump_processes(void) {
+
+   console("PID\tParent\tPriority\tStatus\t\t# Kids\tCPUtime\tName\n"); 
+
+   for (int i = 0; i < MAXPROC; i++) {
+
+      console("%d\t", proc_tbl[i].pid);
+      
+      if (proc_tbl[i].parent_proc_ptr == NULL) {
+         console("%d\t", -1);
+      } else {
+         console("%d\t", proc_tbl[i].parent_proc_ptr->pid);
+      }
+
+      console("%d\t\t", proc_tbl[i].priority);
+
+      switch (proc_tbl[i].status) {
+         case 0:
+            console("EMPTY\t\t");
+            break;
+         case 1:
+            console("RUNNING\t\t");
+            break;
+         case 2:
+            console("READY\t\t");
+            break;
+         case 3:
+            console("QUIT\t\t");
+            break;
+         case 4:
+            console("jOIN_BLOCKED\t\t");
+            break;
+         case 5:
+            console("ZAP_BLOCKED\t\t");
+            break;
+         case 6:
+            console("LAST\t\t");
+            break;
+      }
+
+      console("%d\t", proc_tbl[i].children.count);
+      console("%d\t", 0);
+      console("%s\n", proc_tbl[i].name);
+   }
+}
+
+/* TODO */
+int getpid(void) {
+   /* code ..... */
+   return new_pid;
 }
